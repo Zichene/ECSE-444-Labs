8ca33f70a1a5c887a68460f9f95716e81dc33aeb
Zi <zichen.gao2@mail.mcgill.ca>
2023-09-29 15:00:22 -0400

    added ADC temperature reading code

--- Lab 2/Core/Src/main.c
+++ Lab 2/Core/Src/main.c
@@ -21,7 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#define ARM_MATH_CM4
+#include "arm_math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -31,7 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define pi 3.14
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -40,6 +41,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 DAC_HandleTypeDef hdac1;
 
 /* USER CODE BEGIN PV */
@@ -50,6 +53,7 @@ DAC_HandleTypeDef hdac1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DAC1_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 void delay_forloop(int n);
 /* USER CODE END PFP */
@@ -87,13 +91,64 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DAC1_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
+  // setup ADC
+  ADC_ChannelConfTypeDef channel_config_vref = {
+  	  	  .Channel = ADC_CHANNEL_VREFINT,
+		  .Rank = ADC_REGULAR_RANK_1,
+		  .SamplingTime = ADC_SAMPLETIME_640CYCLES_5,
+		  .SingleDiff = ADC_SINGLE_ENDED,
+		  .OffsetNumber = ADC_OFFSET_NONE,
+		  .Offset = 0
+  };
+
+  ADC_ChannelConfTypeDef channel_config_temp = {
+  	  	  .Channel = ADC_CHANNEL_TEMPSENSOR,
+		  .Rank = ADC_REGULAR_RANK_1,
+		  .SamplingTime = ADC_SAMPLETIME_640CYCLES_5,
+		  .SingleDiff = ADC_SINGLE_ENDED,
+		  .OffsetNumber = ADC_OFFSET_NONE,
+		  .Offset = 0
+  };
+
+
+  if (HAL_ADC_ConfigChannel (&hadc1, &channel_config_vref) != HAL_OK){
+       return -1;
+   }
+
+  int *p_TS_CAL1 = (int *)0x1FFF75A8;
+  int16_t TS_CAL1 = *p_TS_CAL1;
+  int16_t TS_CAL1_TEMP = 30;
+
+  int *p_TS_CAL2 = (int *)0x1FFF75CA;
+  int16_t TS_CAL2 = *p_TS_CAL2;
+  int16_t TS_CAL2_TEMP = 110;
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+	  HAL_ADC_Start(&hadc1);
+	  HAL_ADC_PollForConversion(&hadc1, 199); // 199 is timeout in ms
+	  uint32_t vref = HAL_ADC_GetValue(&hadc1);
+	  HAL_ADC_Stop(&hadc1);
+
+	  if (HAL_ADC_ConfigChannel (&hadc1, &channel_config_temp) != HAL_OK){
+	        return -1;
+	    }
+
+	  // temperature
+	  HAL_ADC_Start(&hadc1);
+	  HAL_ADC_PollForConversion(&hadc1, 199); // 199 is timeout in ms
+	  int16_t TS_DATA = HAL_ADC_GetValue(&hadc1);
+	  TS_DATA = TS_DATA*11/10;
+	  HAL_ADC_Stop(&hadc1);
+
+	  int16_t temp = (TS_CAL2_TEMP - TS_CAL1_TEMP)*(TS_DATA-TS_CAL1)/(TS_CAL2 - TS_CAL1) + 30;
+
 	  GPIO_PinState pinState = HAL_GPIO_ReadPin(PB_BLUE_GPIO_Port, PB_BLUE_Pin);
 	  if (pinState == GPIO_PIN_RESET) {
 		  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
@@ -106,19 +161,24 @@ int main(void)
 	 // init signal vars
 	 uint8_t triangle = 0;
 	 uint8_t saw = 0;
+	 uint8_t sine = 0;
 	 // saw wave + triangle wave
 	 while(1) {
 		 for (int i = 0; i <= 7; i++) {
 			 saw = 16*i;
 			 triangle = 32*i;
+			 sine = 128*arm_sin_f32((pi*i) / 7) + 128;
 			 HAL_Delay(1);
-			 HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_8B_R, triangle);
+			 HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_8B_R, sine);
+			 HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_2, DAC_ALIGN_8B_R, triangle);
 		 }
 		 for (int i = 1; i <= 7; i++) {
 			 saw = 16*(7+i);
 			 triangle = 32*(7-i);
+			 sine = 128*arm_sin_f32(pi+((pi*i) / 7)) + 128;
 			 HAL_Delay(1);
-			 HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_2, DAC_ALIGN_8B_R, saw);
+			 HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_8B_R, sine);
+			 HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_2, DAC_ALIGN_8B_R, triangle);
 		 }
 	 }
 
@@ -177,6 +237,74 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.LowPowerAutoWait = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.NbrOfConversion = 2;
+  hadc1.Init.DiscontinuousConvMode = ENABLE;
+  hadc1.Init.NbrOfDiscConversion = 1;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  hadc1.Init.OversamplingMode = DISABLE;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_VREFINT;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
+  sConfig.SingleDiff = ADC_SINGLE_ENDED;
+  sConfig.OffsetNumber = ADC_OFFSET_NONE;
+  sConfig.Offset = 0;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  sConfig.Rank = ADC_REGULAR_RANK_2;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
